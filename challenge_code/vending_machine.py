2c80886f04f3c6aa027b872fe4ba7caa3b05621b
cknight51 <139079736+cknight51@users.noreply.github.com>
2026-06-12 08:34:07 -0500

    Update vending_machine.py

--- challenge_code/vending_machine.py
+++ challenge_code/vending_machine.py
@@ -10,7 +10,7 @@ def main():
     
     while amount_due > 0:
             print(f"Amount Due: {amount_due}")
-            coin = input("Insert Coin: "))
+            coin = input("Insert Coin: ")
             if coin in {"1", "5", "10","25"}:
                 amount_due -= int(coin)
 
